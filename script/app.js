3efcf52e93d8954c6f865a2838daf5b903bb71df
Eman Shafiq <emanshafiq78654@gmail.com>
2023-04-09 03:18:24 +0500

    JS File

---
+++ script/app.js
@@ -0,0 +1,120 @@
+//DAY 1: 6th APRIL
+
+//CHAPTER 1: ALERTS
+
+alert("Thanks for your input!");
+alert ( "Thanks for your input!" );
+
+//CHAPTER 2: VARIABLES FOR STRINGS
+
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+alert("Thanks for your input!");
+ var thanx = "Thanks for your input!"
+ alert(thanx);
+
+ //CHAPTER 3: VARIABLES FOR NUMBERS
+
+ var weight = 150;
+ weight + 25
+ var originalNum = 23;
+ var newNum = originalNum + 7;
+ var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+ var originalNum = 90;
+ originalNum = originalNum + 10;
+ var originalNum = "23";
+ var newNum = originalNum + 7;
+ var originalNumber = 23;
+ var newNumber = originalNumber + 7;
+ var President = "Washington";
+ var prezWhoCame1st = "Washington";
+ alert(144);
+ var caseQty = 144;
+ alert(caseQty);
+
+ //CHAPTER 4: VARIABLES NAMES LEGAL AND ILLEGAL
+
+ userResponse
+userResponseTime
+userResponseTimeLimit
+response
+
+//CHAPTER 5: MATH EXPRESSIONS: FAMILIAR OPERATORS
+
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+var num = 10;
+var popularNumber = num + 200;
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+var whatsLeftOver = 10 % 3;
+var whatsLeftOver = 9 % 3;
+
+//CHAPTER 6: MATH EXPRESSIONS: UNFAMILIAR OPERATORS
+
+num++;
+num = num + 1;
+num--;
+var num = 1;
+var newNum = num++;
+var num = 1;
+var newNum = ++num;
+var num = 1;
+var newNum = num--;
+var num = 1;
+var newNum = --num;
+
+//CHAPTER 7: MATH EXPRESSIONS: ELIMINATING AMBIGUITY
+
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+
+//CHAPTER 8: CONCATENATING TEXT STRINGS
+
+alert("Thanks for your input!");
+var message = "Thanks for your input!";
+alert(message);
+alert("Thanks, " + userName + "!");
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
+alert("Thanks, " + "Susan" + "!");
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+alert("2" + "2");
+alert("2 plus 2 equals " + 2 + 2);
+
+//CHAPTER 9: PROMPTS
+
+var spec = prompt("Your species?", "human");
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
